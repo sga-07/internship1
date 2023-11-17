6300c03470e99c2b5384102bbfd8280a837c2e01
Saumin Das <dassaumin17@gmail.com>
2023-11-16 21:30:29 -0500

    Saumin's changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MesoOn
+# Genio Tech
 Online learning platform builded with Django
 
 ## Setup
@@ -7,5 +7,4 @@ Online learning platform builded with Django
 3. Install required packages: ``` pip install -r requirements.txt ```
 4. Run app: ``` python manage.py runserver ```
 
-## Certificate
-This project is officially certificated by **Redian Marku** the creator of this project.
+
--- courses/forms.py
+++ courses/forms.py
@@ -9,9 +9,9 @@ class KlasaForm(forms.ModelForm):
         model = Klasa
         fields = '__all__'
         help_texts = {
-            'titulli': 'Psh. Klasa 11 ose Klasa e Informatikes',
-            'pershkrimi':'Vendos nje pershkrim te shkurte te klases',
-            'imazhi':'Mund te vendosesh nje fotografi e klases ose mund te lihet bosh'
+            'titulli': 'e.g., Class 11 or Computer Science Class',
+            'pershkrimi':'Enter a brief description of the class.',
+            'imazhi':'You can upload a photo of the class or leave it blank'
         }
 
 class LendaForm(forms.ModelForm):
@@ -19,13 +19,13 @@ class LendaForm(forms.ModelForm):
         model = Lendet
         fields = ['krijues','slug', 'titulli', 'klasa', 'pershkrimi', 'imazhi_lendes']
         help_texts = {
-            'titulli': 'Psh. Matematika, Gjeografi etj',
-            'pershkrimi':'Vendos nje pershkrim te shkurte te lendes',
-            'klasa':'Zhgjidhni klasen per te cilen do te krijoni lenden',
-            'imazhi_lendes':'Mund te vendosesh nje fotografi e lendes ose mund te lihet bosh'
+            'titulli': 'E.g., Arduino Kit, Programming, etc',
+            'pershkrimi':'Enter a brief description of the subject',
+            'klasa':'Choose the class for which you will create the subject',
+            'imazhi_lendes':'You can upload a photo of the subject, or it can be left blank'
         }
         labels = {
-            'titulli':'Titulli i lendes'
+            'titulli':'Subject Title'
         }
         widgets = {'krijues': forms.HiddenInput(), 'slug': forms.HiddenInput()}
 
@@ -35,10 +35,10 @@ class MesimiForm(forms.ModelForm):
         model = Lesson 
         fields = ['slug','titulli', 'lenda', 'video_id', 'pozicioni', ]
         help_texts = {
-            'titulli':'Vendosni titullin e mesimit',
-            'lenda':'Zgjidhni lenden per te cilen i perket ky mesim',
-            'video_id':'Vendosni ID e videos nga Youtube te cilen do te ngarkoni (<a href="/media/youtube_help.png">ku mund ta gjej ID</a>)',
-            'pozicioni':'Vendosni numrin e pozicionit ose radhen e mesimit '
+            'titulli':'Enter the lesson title',
+            'lenda':'Choose the subject to which this lesson belongs',
+            'video_id':'Enter the YouTube video ID that you will upload" (<a href="/media/youtube_help.png">ku mund ta gjej ID</a>)',
+            'pozicioni':'Enter the lesson number or sequence'
         }
         widgets = {
             'slug': forms.HiddenInput()
