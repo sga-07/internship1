57e83f541f39043cb41f9d9ff574ddd97962bb2a
Saumin Das <dassaumin17@gmail.com>
2023-11-17 19:21:10 -0500

    Saumin Created and Fixed Create class, subject and lesson page as well as forms.py

--- courses/forms.py
+++ courses/forms.py
@@ -10,8 +10,13 @@ class KlasaForm(forms.ModelForm):
         fields = '__all__'
         help_texts = {
             'titulli': 'e.g., Class 11 or Computer Science Class',
-            'pershkrimi':'Enter a brief description of the class.',
-            'imazhi':'You can upload a photo of the class or leave it blank'
+            'pershkrimi': 'Enter a brief description of the class.',
+            'imazhi': 'You can upload a photo of the class or leave it blank'
+        }
+        labels = {
+            'titulli' : 'Title',
+            'pershkrimi' : 'Description',
+            'imazhi' : 'Image'
         }
 
 class LendaForm(forms.ModelForm):
@@ -19,27 +24,39 @@ class LendaForm(forms.ModelForm):
         model = Lendet
         fields = ['krijues','slug', 'titulli', 'klasa', 'pershkrimi', 'imazhi_lendes']
         help_texts = {
-            'titulli': 'E.g., Arduino Kit, Programming, etc',
+            'titulli': 'E.g....., Arduino Kit, Programming, etc',
             'pershkrimi':'Enter a brief description of the subject',
             'klasa':'Choose the class for which you will create the subject',
             'imazhi_lendes':'You can upload a photo of the subject, or it can be left blank'
         }
         labels = {
-            'titulli':'Subject Title'
+            'krijues' : 'Creator',
+            'titulli':'Subject Title',
+            'klasa' : 'Class',
+            'pershkrimi' : 'Description',
+            'imazhi_lendes' : 'Image-materials'
+
         }
-        widgets = {'krijues': forms.HiddenInput(), 'slug': forms.HiddenInput()}
+        # widgets = {'krijues': forms.HiddenInput(), 'slug': forms.HiddenInput()}
 
 
 class MesimiForm(forms.ModelForm):
     class Meta:
-        model = Lesson 
+        model = Lesson
         fields = ['slug','titulli', 'lenda', 'video_id', 'pozicioni', ]
         help_texts = {
             'titulli':'Enter the lesson title',
             'lenda':'Choose the subject to which this lesson belongs',
-            'video_id':'Enter the YouTube video ID that you will upload" (<a href="/media/youtube_help.png">ku mund ta gjej ID</a>)',
+            'video_id':'Enter the zoom meeting link here ',
             'pozicioni':'Enter the lesson number or sequence'
         }
-        widgets = {
-            'slug': forms.HiddenInput()
+        labels = {
+            'titulli': 'Lesson Title',
+            'lenda' : 'Subject',
+            'pozicioni' : 'Position',
+            'video_id' : 'Zoom Link'
+
         }
+        # widgets = {
+        #     'slug': forms.HiddenInput()
+        # }
